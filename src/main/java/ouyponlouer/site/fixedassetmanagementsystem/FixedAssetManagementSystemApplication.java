6cd9c9f120eb45425124c2b47d7bbe46f7a18846
Hyouka898 <ouyponlouer@gmail.com>
2025-12-17 10:13:43 +0700

    first commit fixed asset

---
+++ src/main/java/ouyponlouer/site/fixedassetmanagementsystem/FixedAssetManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package ouyponlouer.site.fixedassetmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixedAssetManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FixedAssetManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ouyponlouer/site/fixedassetmanagementsystem/FixedAssetManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ouyponlouer.site.fixedassetmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixedAssetManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
